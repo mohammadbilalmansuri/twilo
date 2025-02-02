f94b4e38e3a537f7bab8592a05796fd282ed5580
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-02-02 15:06:23 +0530

    refactor(store): simplify action payload destructuring in auth and post slices; remove unused user slice

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice";
 import postReducer from "./postSlice";
-import userReducer from "./userSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     post: postReducer,
-    user: userReducer,
   },
   devTools: process.env.NODE_ENV !== "production",
 });
