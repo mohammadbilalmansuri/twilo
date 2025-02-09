53da5126038d769de8fb4762d2ef0bfbabacd099
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-02-09 23:59:59 +0530

    refactor: improve conditional rendering and readability in Feed and Profile components

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -30,15 +30,13 @@ const Feed = () => {
         <p className="max-w my-auto sm:text-2xl text-xl font-semibold leading-tight text-center py-4 text-black/60">
           No posts available in the feed at the moment.
         </p>
-      ) : (
-        posts && (
-          <div className="max-w relative flex flex-col items-center lg:py-4 py-3 lg:gap-4 gap-3">
-            <PostMasonry posts={posts} page="feed" lastPostRef={lastPostRef} />
+      ) : posts ? (
+        <div className="max-w relative flex flex-col items-center lg:py-4 py-3 lg:gap-4 gap-3">
+          <PostMasonry posts={posts} page="feed" lastPostRef={lastPostRef} />
 
-            {loading && hasMore && <Loader size="md" />}
-          </div>
-        )
-      )}
+          {loading && hasMore && <Loader size="md" />}
+        </div>
+      ) : null}
     </>
   );
 };
