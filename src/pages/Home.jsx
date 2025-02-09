46c4a5fdeb1360f27af7c967e6c0860ba8884207
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-02-09 07:45:41 +0530

    make home page responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,12 +12,12 @@ const Home = () => {
         />
       </Helmet>
 
-      <div className="wrapper-center gap-4 text-center py-8">
-        <h4 className="h4 border-b-1.5 border-b-black/10 pb-0.5">
+      <div className="max-w min-h-inherit relative flex flex-col items-center justify-center gap-4 text-center py-8">
+        <h4 className="md:text-lg xs:text-base text-sm leading-tight border-b-1.5 border-b-black/10 pb-0.5">
           Start your blogging journey today with Twilo
         </h4>
         <h1 className="h1">Bring Your Stories and Ideas to Life</h1>
-        <p className="text max-w-xl">
+        <p className="text text-black/60 md:max-w-xl max-w-lg">
           Twilo is your go-to platform for easy blogging. Share your stories,
           ideas, and creativity with a lively community of readers. Whether
           you're writing for fun, exploring new ideas, or growing your audience,
