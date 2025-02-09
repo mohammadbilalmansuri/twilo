0c082c3426ed4fc334d8e5f1fcba26af42ba3d72
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-02-09 17:43:28 +0530

    feat: enhance PostCard layout by aligning items in the header section

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -14,7 +14,7 @@ const PostCard = forwardRef(
         onClick={() => navigate(`/post/${$id}`)}
         className="w-full relative border-1.5 border-black/10 rounded-lg transition-all hover:border-black/60 flex flex-col cursor-pointer lg:p-4 p-3 gap-3 break-words"
       >
-        <div className="w-full flex justify-between gap-4">
+        <div className="w-full flex justify-between items-center gap-4">
           <button
             className="group sm:text-lg text-base leading-none text-blue font-medium transition-all flex gap-2 items-center"
             {...(page === "feed" && {
