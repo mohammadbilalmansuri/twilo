a3a36ddf96c05ad14f1637908fbf57aef54f7bd3
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-02-09 15:28:32 +0530

    feat: reorganize imports in index files for improved clarity and structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,29 @@
 import Home from "./Home";
+import Login from "./Login";
+import Signup from "./Signup";
+import Verification from "./Verification.jsx";
+import Verify from "./Verify.jsx";
+import ForgotPassword from "./ForgotPassword.jsx";
+import ResetPassword from "./ResetPassword.jsx";
 import Feed from "./Feed";
 import CreatePost from "./CreatePost";
 import Post from "./Post";
 import EditPost from "./EditPost";
 import Profile from "./Profile";
-import Login from "./Login";
-import Signup from "./Signup";
 import NotFound from "./NotFound.jsx";
-import Verification from "./Verification.jsx";
-import Verify from "./Verify.jsx";
-import ResetPassword from "./ResetPassword.jsx";
-import ForgotPassword from "./ForgotPassword.jsx";
 
 export {
   Home,
+  Login,
+  Signup,
+  Verification,
+  Verify,
+  ForgotPassword,
+  ResetPassword,
   Feed,
   CreatePost,
   Post,
   EditPost,
   Profile,
-  Login,
-  Signup,
   NotFound,
-  Verification,
-  Verify,
-  ResetPassword,
-  ForgotPassword,
 };
