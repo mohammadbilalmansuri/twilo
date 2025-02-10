4a548cde431e7544487801df0ce9af93c7508834
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-02-10 10:36:58 +0530

    fix: remove bottom border from Footer component layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="w-full relative flex flex-col items-center lg:px-4 px-3">
-      <div className="max-w relative p-3 flex gap-3 justify-between items-center border-1.5 border-black/10 lg:border-b-0 rounded-t-lg">
+      <div className="max-w relative p-3 flex gap-3 justify-between items-center border-1.5 border-black/10 border-b-0 rounded-t-lg">
         <p className="sm:text-lg xs:text-base text-sm leading-snug mt-0.5">
           ©2025 Twilo | All Rights Reserved.
         </p>
